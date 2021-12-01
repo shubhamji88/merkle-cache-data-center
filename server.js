ccf99f6baabd8a0da3c0fbe83135cd5b6b30ae20
Yash Kumar Verma <yash.verma@fampay.in>
2021-12-01 09:25:27 +0530

    feat: fetch new file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { promisify } = require('util');
 const exec = promisify(require('child_process').exec)
 const express = require("express")
 const app = express()
+const fs = require('fs');
 
 const markle = async function makeTree () {
     // Exec output contains both stderr and stdout outputs
@@ -23,6 +24,14 @@ app.get("/tree", async (req, res)=>{
     return res.json(parsed_json)
 })
 
+
+app.get("/new/:filename", async (req, res) => {
+    const {filename} = req.params;
+
+    // load the contents of the file using fs module
+    const data = await fs.readFileSync(`data/${filename}`, 'utf8');
+    return res.send(data)
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port http://localhost:3000")
