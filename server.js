d79b97ed017fec92313b3a418322865064adfe79
Yash Kumar Verma <yash.verma@fampay.in>
2021-12-01 09:20:45 +0530

    feat: generate merkle tree

---
+++ server.js
@@ -0,0 +1,29 @@
+
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec)
+const express = require("express")
+const app = express()
+
+const markle = async function makeTree () {
+    // Exec output contains both stderr and stdout outputs
+    const command = await exec('python2 mt.py')
+  
+    return { 
+      stdout: command.stdout.trim(), 
+    }
+  };
+
+app.get("/",(req, res)=>{
+    return res.json({alive:true})
+})
+
+app.get("/tree", async (req, res)=>{
+    const data = await markle()
+    const parsed_json = JSON.parse(data.stdout)
+    return res.json(parsed_json)
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port http://localhost:3000")
+})
